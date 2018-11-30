74f43d71d2c6551d07aa60692450b47d7e99779c
ONE <ONE@DESKTOP-CK0MR44>
2018-11-30 11:12:22 +0800

    免费课程

--- SSM/src/main/java/com/ssm/sample/controller/wkx/FreeLessonController.java
+++ SSM/src/main/java/com/ssm/sample/controller/wkx/FreeLessonController.java
@@ -1,36 +1,58 @@
 package com.ssm.sample.controller.wkx;
+
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.ssm.sample.controller.base.BaseController;
 import com.ssm.sample.facade.classes.ClassesFacade;
 import com.ssm.sample.util.PageData;
+
 @Controller
 @RequestMapping({ "/course" })
-public class FreeLessonController  extends BaseController{	
-	
+public class FreeLessonController extends BaseController {
+
 	@Autowired
 	ClassesFacade classesFacade;
-	
+
 	/*
 	 * 免费课程
 	 */
 	@RequestMapping(value = "/list")
-	public ModelAndView home() {
-		ModelAndView mv = this.getModelAndView();					
+	public ModelAndView home(@RequestParam(value = "pn", defaultValue = "1") Integer pn) {
+		ModelAndView mv = this.getModelAndView();
 		mv.setViewName("wkx/freeclass");
-		
+
 		try {
 			PageData pd = this.getPageData();
+			PageHelper.startPage(pn, 30);
 			List<PageData> list = this.classesFacade.getAllClasses(pd);
-			mv.addObject("classes", list);
+			PageInfo page = new PageInfo(list);
+			session.setAttribute("classes", page);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return mv;
+	}
+
+	@ResponseBody
+	@RequestMapping("/selectCLassByClassType")
+	public Object selectCLassByClassType() {
+		PageData pd = this.getPageData();
+
+		PageHelper.startPage(1, 30);
+		List<PageData> list = this.classesFacade.selectCLassByClassType(pd);
+		PageInfo page = new PageInfo(list);
+		session.setAttribute("classes", page);
+
+		return null;
 	}
 }
